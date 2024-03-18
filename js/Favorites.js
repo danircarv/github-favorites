43dd9d88aa6aa4403984750741191152328ad0e5
daniel <danielrodriguescarvalho777@gmail.com>
2024-03-18 18:10:10 -0300

    feature: delete user

--- js/Favorites.js
+++ js/Favorites.js
@@ -28,6 +28,16 @@ export class Favorites {
       },
     ];
   }
+
+  delete(user) {
+    // Higher order functions (map, filter, find, reduce)
+    const filteredEntries = this.entries.filter(
+      (entry) => entry.login !== user.login
+    );
+
+    this.entries = filteredEntries;
+    this.update();
+  }
 }
 
 //classe que vai criar a visualização e eventos do HTML
@@ -53,6 +63,13 @@ export class FavoritesView extends Favorites {
       row.querySelector(".user span").textContent = user.login;
       row.querySelector(".repositories").textContent = user.public_repos;
       row.querySelector(".followers").textContent = user.followers;
+
+      row.querySelector(".remove").onclick = () => {
+        const isOk = confirm("Tem certeza que deseja deletar essa linha?");
+        if (isOk) {
+          this.delete(user);
+        }
+      };
 
       this.tbody.append(row);
     });
